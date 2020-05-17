21a9b45315fd77812349c6e931ea5ad88411f8c6
julien <julien.fruteau@gmail.com>
2020-05-17 18:02:12 +0200

    Enemy Behaviour

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 4f;
+
+    private Vector3 _direction = Vector3.down;
+
+    [SerializeField]
+    private float _ySpawn = 7f;
+
+    [SerializeField]
+    private float _yMin = -6f;
+
+    [SerializeField]
+    private float _xMin = -9f;
+
+    [SerializeField]
+    private float _xMax = 9f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(_direction * _speed * Time.deltaTime);
+    
+        if (transform.position.y < _yMin)
+        {
+            transform.position = new Vector3(Random.Range(_xMin, _xMax), _ySpawn, 0);
+        }    
+    }
+
+}
