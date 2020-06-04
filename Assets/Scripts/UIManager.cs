1bb4c5f41dcf6967fa3ca0c1a836d1b8caf1da95
julien <julien.fruteau@gmail.com>
2020-06-04 07:41:13 +0200

    impl flickr game over + restart (load scene)

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,7 +1,10 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
@@ -9,17 +12,29 @@ public class UIManager : MonoBehaviour
     [SerializeField] private Sprite[] _liveSprites;
     [SerializeField] private Image _livesImage;
     [SerializeField] private Text _gameOverText;
+    [SerializeField] private Text _restartGameText;
+    private PlayerControl _inputAction;
+    private bool _restartGame = false;
 
-    // Start is called before the first frame update
+    private void Awake() {
+        _inputAction = new PlayerControl();
+        // _inputAction.Player.RestartGame.performed += context => _restartGame = context.ReadValueAsButton();
+    }
+
     void Start()
     {
         UpdateScore(0);
         _gameOverText.gameObject.SetActive(false);
+        _restartGameText.gameObject.SetActive(false);
     }
 
-    // void Update()
-    // {
-    // }
+    void Update()
+    {
+        if (_restartGame)
+        {
+            SceneManager.LoadScene("Scenes/SampleScene", LoadSceneMode.Single);
+        }
+    }
 
     public void UpdateScore(int playerScore)
     {
@@ -30,6 +45,40 @@ public class UIManager : MonoBehaviour
     {
         _livesImage.sprite = _liveSprites[currentLives];
         if (currentLives == 0)
+        {
+            _restartGameText.gameObject.SetActive(true);
+            StartCoroutine(DisplayGameOverRoutine());
+
             _gameOverText.gameObject.SetActive(true);
+            _inputAction.Player.RestartGame.performed += context => _restartGame = context.ReadValueAsButton();
+        }
+            
+    }
+
+    private IEnumerator DisplayGameOverRoutine()
+    {
+        while (true)
+        {
+            _gameOverText.text = "GAME OVER";
+            yield return new WaitForSeconds(.5f);
+            _gameOverText.text = "";
+            yield return new WaitForSeconds(.5f);
+        }
+    }
+
+    private void OnEnable()
+    {
+        if (_inputAction != null)
+        {
+            _inputAction.Enable();
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (_inputAction != null)
+        {
+            _inputAction.Disable();
+        }
     }
 }
