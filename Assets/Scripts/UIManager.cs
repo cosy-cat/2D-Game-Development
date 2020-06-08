5710e736850f1c6832c6ea2a74b819ca27632548
julien <julien.fruteau@gmail.com>
2020-06-08 06:33:15 +0200

    add GameManager to handle reloadScene

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.InputSystem;
-using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
@@ -13,28 +12,24 @@ public class UIManager : MonoBehaviour
     [SerializeField] private Image _livesImage;
     [SerializeField] private Text _gameOverText;
     [SerializeField] private Text _restartGameText;
-    private PlayerControl _inputAction;
-    private bool _restartGame = false;
-
-    private void Awake() {
-        _inputAction = new PlayerControl();
-        // _inputAction.Player.RestartGame.performed += context => _restartGame = context.ReadValueAsButton();
-    }
+    private GameManager _gameManager;
 
     void Start()
     {
         UpdateScore(0);
         _gameOverText.gameObject.SetActive(false);
         _restartGameText.gameObject.SetActive(false);
-    }
+        _gameManager = GameObject.Find("Game_Manager").GetComponent<GameManager>();
 
-    void Update()
-    {
-        if (_restartGame)
+        if ( _gameManager == null)
         {
-            SceneManager.LoadScene("Scenes/SampleScene", LoadSceneMode.Single);
+            Debug.LogError("Game Manager is null");
         }
     }
+
+    // void Update()
+    // {
+    // }
 
     public void UpdateScore(int playerScore)
     {
@@ -55,7 +50,7 @@ public class UIManager : MonoBehaviour
         _restartGameText.gameObject.SetActive(true);
         StartCoroutine(DisplayGameOverRoutine());
         _gameOverText.gameObject.SetActive(true);
-        _inputAction.Player.RestartGame.performed += context => _restartGame = context.ReadValueAsButton();
+        _gameManager.GameOver();
     }
 
     private IEnumerator DisplayGameOverRoutine()
@@ -66,22 +61,6 @@ public class UIManager : MonoBehaviour
             yield return new WaitForSeconds(.5f);
             _gameOverText.text = "";
             yield return new WaitForSeconds(.5f);
-        }
-    }
-
-    private void OnEnable()
-    {
-        if (_inputAction != null)
-        {
-            _inputAction.Enable();
-        }
-    }
-
-    private void OnDisable()
-    {
-        if (_inputAction != null)
-        {
-            _inputAction.Disable();
         }
     }
 }
