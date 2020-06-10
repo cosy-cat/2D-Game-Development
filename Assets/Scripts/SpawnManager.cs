248f30cd93385275b9cb9340aa5165e8df2fd481
julien <julien.fruteau@gmail.com>
2020-06-10 06:55:19 +0200

    fix: player death during asteroid spawn loop causes crash

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -66,7 +66,8 @@ public class SpawnManager : MonoBehaviour
             yield return new WaitForSeconds(UnityEngine.Random.Range(_spawnMinDelay, _spawnMaxDelay));
             for (int i = 0; i < UnityEngine.Random.Range(1, 4); i++)
             {
-                Instantiate(_asteroidPrefab, GetSpawnObjectLocation(), Quaternion.identity);
+                if (!_stopSpawning)
+                    Instantiate(_asteroidPrefab, GetSpawnObjectLocation(), Quaternion.identity);
                 yield return new WaitForSeconds(UnityEngine.Random.Range(.2f, 1f));
             }
         }
